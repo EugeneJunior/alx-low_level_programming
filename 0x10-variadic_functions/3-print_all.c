704d44ad2589c8489f0800f7f29befaed4e9ad62
EugeneJunior <lapostaadjei@gmail.com>
2023-01-21 10:54:27 +0000

    This is my ALX variadic functions project

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,71 @@
+#include "variadic_functions.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_all - print anything
+ * @format: string of types to print
+ * @...: variable number of non-keyword arguments
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+
+	char *k, *v = "\0";
+
+	va_list args;
+
+	j = strlen(format);
+	i = 0;
+	va_start(args, format);
+	while (i != (j - 1))
+	{
+		switch ((char) format[i])
+		{
+			case 'c':
+				printf("%c, ", va_arg(args, int));
+				break;
+			case 's':
+				k = va_arg(args, char *);
+				if (*k != *v)
+					printf("%s, ", k);
+				else
+					printf("(nil), ");
+				break;
+			case 'f':
+				printf("%f, ", va_arg(args, double));
+				break;
+			case 'i':
+				printf("%d, ", va_arg(args, int));
+				break;
+			default:
+				break; }
+		++i;
+	}
+	while (i == (j - 1))
+	{
+		switch ((char) format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 's':
+				k = va_arg(args, char *);
+				if (*k != *v)
+					printf("%s", k);
+				else
+					printf("(nil)");
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 'i':
+				printf("%d ", va_arg(args, int));
+				break;
+			default:
+				break; }
+		++i; }
+	va_end(args);
+	printf("\n");
+}
