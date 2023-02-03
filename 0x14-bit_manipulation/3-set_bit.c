ea299fbf65b062104332130452ac404bf00bebba
EugeneJunior <lapostaadjei@gmail.com>
2023-02-03 05:45:22 +0000

    This is my ALX bit manipulation project

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,12 +4,15 @@
  * @n: the whose bit we want to set
  * @index: position of the bit we want to set
  *
- * Return: returns -1 if bit setting fails
+ * Return: returns -1 if bit setting fails, 1 if success, 0 if n is NULL
  *
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	if (n == NULL)
+		return (0);
+
 	*n = (1 << index) | *n;
 	if (*n << index & 1)
 		return (1);
